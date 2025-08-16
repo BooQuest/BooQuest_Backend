e1096a87d6331cc6995b1687c49ece12cef21a8b
김기웅 <kko2746@nate.com>
2025-08-16 20:40:46 +0900

    feat 선택한 부업에 대한 mission 5개 생성

--- booquest-api/src/main/java/com/booquest/booquest_api/application/port/out/sidejob/SideJobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.booquest.booquest_api.application.port.out.sidejob;
-
-import com.booquest.booquest_api.domain.sidejob.model.SideJob;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SideJobRepository extends JpaRepository<SideJob, Long> {
-
-}
