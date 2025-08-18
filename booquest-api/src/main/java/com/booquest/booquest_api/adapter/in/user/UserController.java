8f76e31b044ab1a77ea4883a9c410074558dad64
cyjworks <cyj.engineer@gmail.com>
2025-08-18 10:44:52 +0900

    feat: accessToken 검증 후 사용자 정보 반환 API 구현

---
+++ booquest-api/src/main/java/com/booquest/booquest_api/adapter/in/user/UserController.java
@@ -0,0 +1,28 @@
+package com.booquest.booquest_api.adapter.in.user;
+
+import com.booquest.booquest_api.adapter.out.user.dto.UserResponse;
+import com.booquest.booquest_api.application.port.in.user.GetUserUseCase;
+import com.booquest.booquest_api.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final GetUserUseCase getUserUseCase;
+
+    @GetMapping("/me")
+    public ApiResponse<UserResponse> getUserFromAuth() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserResponse response = getUserUseCase.getUserFromAuth(auth);
+        return ApiResponse.success("사용자 조회에 성공했습니다.", response);
+    }
+}
--- booquest-api/src/main/java/com/booquest/booquest_api/application/service/user/UserService.java
+++ booquest-api/src/main/java/com/booquest/booquest_api/application/service/user/UserService.java
@@ -1,19 +1,36 @@
 package com.booquest.booquest_api.application.service.user;
 
+import com.booquest.booquest_api.adapter.out.user.dto.UserResponse;
+import com.booquest.booquest_api.application.port.in.user.GetUserUseCase;
 import com.booquest.booquest_api.application.port.in.user.UpdateUserProfileUseCase;
 import com.booquest.booquest_api.application.port.out.user.UserCommandPort;
 import com.booquest.booquest_api.application.port.out.user.UserQueryPort;
 import com.booquest.booquest_api.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements UpdateUserProfileUseCase {
+public class UserService implements GetUserUseCase, UpdateUserProfileUseCase {
 
     private final UserQueryPort userQueryPort;
     private final UserCommandPort userCommandPort;
+
+    @Override
+    public UserResponse getUserById(Long userId) {
+        User user = userQueryPort.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        return UserMapper.toResponse(user);
+
+    }
+
+    @Override
+    public UserResponse getUserFromAuth(Authentication auth) {
+        Long userId = Long.parseLong(auth.getName());
+        return getUserById(userId);
+    }
 
     @Override
     @Transactional
