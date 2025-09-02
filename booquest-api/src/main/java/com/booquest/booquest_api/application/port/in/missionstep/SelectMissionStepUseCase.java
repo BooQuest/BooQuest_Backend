d2dd8466e7c5c5d7db87a5254adaeec5b89b16a9
subin <eltm521@naver.com>
2025-09-02 18:34:40 +0900

    feat : 부퀘스트 생성시 생성된 데이터가 있으면 해당 값을 얼리리턴

--- booquest-api/src/main/java/com/booquest/booquest_api/application/port/in/missionstep/SelectMissionStepUseCase.java
+++ booquest-api/src/main/java/com/booquest/booquest_api/application/port/in/missionstep/SelectMissionStepUseCase.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface SelectMissionStepUseCase {
     MissionStep selectMissionStep(Long stepId);
+    List<MissionStep> selectMissionStepsByMissionId(Long missionId);
 }
